0f7aa2658e0ee8feb2e21a9274d2df1f772f3aba
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-10-04 19:03:11 -0500

    Added angular source, added nav, home, register, login, profile and dashboard components, added route module and routes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MEAN-Social
-Tutorial on MEAN Stack WebApp development
+## Tutorial on MEAN Stack WebApp development
+
+This is a **Node JS** and **Express** based authentication API with an **Angular 2** frontend.
--- routes/Users.js
+++ routes/Users.js
@@ -148,6 +148,32 @@ router.get('/profile', pp.authenticate('jwt-bearer', { session: false }), functi
     //res.send("User Profile");
 });
 
+//Saving posts
+router.post('/post', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+//Updating posts
+router.post('/post/:id?type=up', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+//Deleting posts
+router.post('/post/:id?type=del', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+
+//Saving post comments
+router.post('/comment', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+//Updating post comments
+router.post('/comment/:id?type=up', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+//Deleting post comments
+router.delete('/comment/:id?type=del', pp.authenticate('jwt-bearer', { session: false }), function(req, res, next) {
+
+});
+
 //Validate
 // router.get('/validate', function(req, res, next) {
 
