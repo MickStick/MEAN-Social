dd6cc2899b4903bf55104fd8855249db431bed71
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-10-05 23:29:42 -0500

    coded and styled homepage, created variables for registerCompanent and half a register submit function, created a register form, created validation service

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { ValidateService} from './services/validate.service';
+
 const appRoutes: Routes = [
   {
     path: '',
@@ -51,7 +53,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  fname: String;
+  lname: String;
+  uname: String;
+  email: String;
+  pword: String;
+  rpword: String;
 
   constructor() { }
 
+
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+   const user = {
+     fname: this.fname,
+     lname: this.lname,
+     uname: this.uname,
+     email: this.email,
+     pword: this.pword
+   };
   }
 
 }
